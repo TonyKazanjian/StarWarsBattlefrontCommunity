70de656a98bf9d92397816bbd2e56e620c473725
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-12-21 21:13:07 -0800

    Viewpager tabs and header for image are looking good. Resources imported, but I don't know how to get the different image to appear for each fragment

--- app/src/main/java/com/codementor/android/starwarsbattlefrontcommunity/CommunityFragment.java
+++ app/src/main/java/com/codementor/android/starwarsbattlefrontcommunity/CommunityFragment.java
@@ -1,12 +1,14 @@
 package com.codementor.android.starwarsbattlefrontcommunity;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.codementor.android.starwarsbattlefrontcommunity.view.TopicPagerAdapter;
 
@@ -17,20 +19,28 @@ public class CommunityFragment extends Fragment {
 
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private ImageView mBackgroundImage;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.fragment_community,container,false);
+        View view = inflater.inflate(R.layout.fragment_community, container, false);
+
+        mBackgroundImage = (ImageView)view.findViewById(R.id.htab_header);
+
+        mCollapsingToolbarLayout = (CollapsingToolbarLayout)view.findViewById(R.id.htab_collapse_toolbar);
 
         mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
         TopicPagerAdapter topicPagerAdapter = new TopicPagerAdapter(getActivity().getSupportFragmentManager());
+
         topicPagerAdapter.addFragment(new TopicFragmentHeroHunt(), "Hero Hunt");
-        topicPagerAdapter.addFragment(new TopicFragmentSupremacy(),"Supremacy");
+        topicPagerAdapter.addFragment(new TopicFragmentDroidRun(),"Droid Run");
         topicPagerAdapter.addFragment(new TopicFragmentWalkerAssault(), "Walker Assault");
         mViewPager.setAdapter(topicPagerAdapter);
 
         mTabLayout = (TabLayout) view.findViewById(R.id.tabs);
         mTabLayout.setupWithViewPager(mViewPager);
+
 
         return view;
     }

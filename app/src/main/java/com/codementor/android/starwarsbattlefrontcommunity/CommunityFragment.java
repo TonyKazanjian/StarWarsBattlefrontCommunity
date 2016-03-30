1293f0a591e88208b395dce9e021394859be7289
Tony Kazanjian <tony.kazanjian@gmail.com>
2016-03-30 00:37:01 -0700

    API call works for Topics, yet problems with onPageChangedListener

--- app/src/main/java/com/codementor/android/starwarsbattlefrontcommunity/CommunityFragment.java
+++ app/src/main/java/com/codementor/android/starwarsbattlefrontcommunity/CommunityFragment.java
@@ -2,12 +2,14 @@ package com.codementor.android.starwarsbattlefrontcommunity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,23 +25,22 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 /**
  * Created by tonyk_000 on 12/13/2015.
  */
 public class CommunityFragment extends Fragment {
 
-    private TabLayout mTabLayout;
+    private static final String TAG = "CommunityFragment";
+
+    private Topic mTopic;
+    private TopicFragment mTopicFragment;
+
     private ViewPager mViewPager;
     private ImageView mBackgroundImage;
-    private Toolbar mToolbar;
-
-    private Topic walkerAssault;
-    private Topic blast;
-    private Topic heroesVsVillains;
-
-    private TopicFragment droidRunFragment;
-    private TopicFragment heroHuntFragment;
-    private TopicFragment walkerAssaultFragment;
 
     private TopicPagerAdapter mTopicPagerAdapter;
 
@@ -59,62 +60,88 @@ public class CommunityFragment extends Fragment {
         if (savedInstanceState != null){
             mTopicPage = (int) savedInstanceState.get(EXTRA_TOPIC_PAGE_POSITION);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_community, container, false);
 
-        walkerAssault = new Topic();
-        blast = new Topic();
-        heroesVsVillains = new Topic();
-
-        walkerAssault.setPost(populateWalkerAssault());
-        blast.setPost(populateBlast());
-        heroesVsVillains.setPost(populateHeroesVsVillains());
-
-        mToolbar = (Toolbar) view.findViewById(R.id.toolbar);
-        mToolbar.setVisibility(View.INVISIBLE);
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        toolbar.setVisibility(View.INVISIBLE);
 
         mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
         mTopicPagerAdapter = new TopicPagerAdapter(getActivity().getSupportFragmentManager());
 
-        droidRunFragment = TopicFragment.newInstance(walkerAssault);
-        heroHuntFragment = TopicFragment.newInstance(blast);
-        walkerAssaultFragment = TopicFragment.newInstance(heroesVsVillains);
-
-        mTopicPagerAdapter.addFragment(droidRunFragment, walkerAssault.getTitle(),
-                walkerAssault.getImage_url());
-        mTopicPagerAdapter.addFragment(heroHuntFragment, blast.getTitle(),
-                blast.getImage_url());
-        mTopicPagerAdapter.addFragment(walkerAssaultFragment, heroesVsVillains.getTitle(),
-                heroesVsVillains.getImage_url());
-
         mViewPager.setAdapter(mTopicPagerAdapter);
 
-        mTabLayout = (TabLayout) view.findViewById(R.id.tabs);
-        mTabLayout.setupWithViewPager(mViewPager);
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
         mBackgroundImage = (ImageView) view.findViewById(R.id.htab_header);
-//        mBackgroundImage.setImageResource(mTopicPagerAdapter.getBackgroundImage(0));
-        Picasso.with(getActivity()).load(mTopicPagerAdapter.getBackgroundImage(0)).into(mBackgroundImage);
 
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        final ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+                Log.i(TAG, "Page was scrolled");
             }
 
             @Override
             public void onPageSelected(int position) {
-
-                mBackgroundImage.setImageResource(Integer.parseInt(mTopicPagerAdapter.getBackgroundImage(position)));
+                mBackgroundImage.setImageURI(Uri.parse(mTopicPagerAdapter.getBackgroundImage(position)));
                 mTopicPage = mViewPager.getCurrentItem();
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
 
+            }
+        };
+
+        //creates a REST adaper which points to the Battlefront API endpoint
+        BattlefrontClient client = APIServiceGenerator.createService(BattlefrontClient.class);
+        Call<List<Topic>> call = client.getTopics();
+
+        call.enqueue(new Callback<List<Topic>>() {
+            @Override
+            public void onResponse(Call<List<Topic>> call, Response<List<Topic>> response) {
+                if (response.isSuccessful()) {
+                    Log.i((TAG), response.body().toString());
+                    for (int i = 0; i < response.body().size(); i++) {
+                        mTopic = response.body().get(i);
+                        mTopicFragment = TopicFragment.newInstance(mTopic);
+                        mTopicPagerAdapter.addFragment(mTopicFragment, mTopic.getTitle(), mTopic.getImage_url());
+                        Picasso.with(getActivity()).load(mTopicPagerAdapter.getBackgroundImage(i)).into(mBackgroundImage);
+                        mTopic.setPost(populateWalkerAssault());
+                        mTopicPagerAdapter.notifyDataSetChanged();
+                        mViewPager.addOnPageChangeListener(pageChangeListener);
+                        pageChangeListener.onPageSelected(response.body().indexOf(mTopic));
+                    }
+                }
+
+//                mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//                    @Override
+//                    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onPageSelected(int position) {
+//
+//                        mBackgroundImage.setImageResource(Integer.parseInt(mTopicPagerAdapter.getBackgroundImage(position)));
+//                        mTopicPage = mViewPager.getCurrentItem();
+//                    }
+//
+//                    @Override
+//                    public void onPageScrollStateChanged(int state) {
+//
+//                    }
+//                });
+            }
+
+            @Override
+            public void onFailure(Call<List<Topic>> call, Throwable t) {
+                Log.i(TAG, t.getMessage());
             }
         });
 
@@ -195,72 +222,5 @@ public class CommunityFragment extends Fragment {
 
         return post;
     }
-
-    public Post populateBlast(){
-
-        List<Comment> comments = new ArrayList<>();
-
-        Post post = new Post(String.valueOf(R.id.thread_title),String.valueOf(R.id.author_name), new Date(),
-                String.valueOf(R.id.post_content), R.id.author_photo, null, comments);
-
-        post.setAuthorPhoto(R.drawable.vader);
-        post.setAuthor(getString(R.string.herohunt_author_name));
-        post.setContent(getString(R.string.herohunt_top_post_content));
-        post.setTitle(getString(R.string.herohunt_thread_title));
-        post.setDate(post.getDate());
-
-        comments.add(new Comment(String.valueOf(R.id.author_name), new Date(), String.valueOf(R.id.post_content), R.id.author_photo, null));
-        comments.get(0).setAuthor(getString(R.string.kyloren_name));
-        comments.get(0).setAuthorPhoto(R.drawable.kyloren);
-        comments.get(0).setContent(getString(R.string.kyloren_comment));
-        comments.get(0).setDate(comments.get(0).getDate());
-
-        comments.add(new Comment(String.valueOf(R.id.author_name), new Date(), String.valueOf(R.id.post_content), R.id.author_photo, null));
-        comments.get(1).setAuthor(getString(R.string.rey_name));
-        comments.get(1).setAuthorPhoto(R.drawable.rey);
-        comments.get(1).setContent(getString(R.string.rey_comment));
-        comments.get(1).setDate(comments.get(0).getDate());
-
-        post.setComments(comments);
-
-        return post;
-    }
-
-    public Post populateHeroesVsVillains(){
-
-        List<Comment> comments = new ArrayList<>();
-
-        Post post = new Post(String.valueOf(R.id.thread_title),String.valueOf(R.id.author_name), new Date(),
-                String.valueOf(R.id.post_content), R.id.author_photo, null, comments);
-
-        post.setAuthorPhoto(R.drawable.hansolo);
-        post.setAuthor(getString(R.string.walkerassault_author_name));
-        post.setContent(getString(R.string.walkerassault_top_post_content));
-        post.setTitle(getString(R.string.walkerassault_thread_title));
-        post.setDate(post.getDate());
-
-        comments.add(new Comment(String.valueOf(R.id.author_name), new Date(), String.valueOf(R.id.post_content), R.id.author_photo, null));
-        comments.get(0).setAuthor(getString(R.string.chewbacca_name));
-        comments.get(0).setAuthorPhoto(R.drawable.choobs);
-        comments.get(0).setContent(getString(R.string.chewbacca_comment));
-        comments.get(0).setDate(comments.get(0).getDate());
-
-        comments.add(new Comment(String.valueOf(R.id.author_name), new Date(), String.valueOf(R.id.post_content), R.id.author_photo, null));
-        comments.get(1).setAuthor(getString(R.string.leia_name));
-        comments.get(1).setAuthorPhoto(R.drawable.leia);
-        comments.get(1).setContent(getString(R.string.leia_comment));
-        comments.get(1).setDate(comments.get(0).getDate());
-
-        comments.add(new Comment(String.valueOf(R.id.author_name), new Date(), String.valueOf(R.id.post_content), R.id.author_photo, null));
-        comments.get(2).setAuthor(getString(R.string.finn_name));
-        comments.get(2).setAuthorPhoto(R.drawable.finn);
-        comments.get(2).setContent(getString(R.string.finn_comment));
-        comments.get(2).setDate(comments.get(0).getDate());
-
-        post.setComments(comments);
-
-        return post;
-    }
-
 }
 

f75ffcf4d13fc65df286dcd8237e498d51863332
Tony Kazanjian <tony.kazanjian@gmail.com>
2016-05-14 21:12:18 -0700

    added facebook login, temporary welcome screen

--- app/src/main/java/com/codementor/android/starwarsbattlefrontcommunity/activities/WelcomeActivity.java
+++ app/src/main/java/com/codementor/android/starwarsbattlefrontcommunity/activities/WelcomeActivity.java
@@ -1,8 +1,28 @@
 package com.codementor.android.starwarsbattlefrontcommunity.activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 
+import com.codementor.android.starwarsbattlefrontcommunity.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by tonykazanjian on 5/13/16.
@@ -10,5 +30,41 @@ import com.facebook.login.widget.LoginButton;
 public class WelcomeActivity extends AppCompatActivity {
 
     LoginButton facebookLogin;
+    SharedPreferences sharedPreferences = null;
 
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        setContentView(R.layout.activity_welcome_screen);
+
+        facebookLogin = (LoginButton) findViewById(R.id.facebook_login_button);
+        facebookLogin.setReadPermissions("email");
+        callbackManager = CallbackManager.Factory.create();
+        facebookLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }

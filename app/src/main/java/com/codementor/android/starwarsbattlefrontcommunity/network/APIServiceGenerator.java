0fba8fbdfc9cb508e42f9bbf7ef07d663eefbe48
Tony Kazanjian <tony.kazanjian@gmail.com>
2016-05-22 18:27:28 -0700

    added authorization code in API service generator

--- app/src/main/java/com/codementor/android/starwarsbattlefrontcommunity/network/APIServiceGenerator.java
+++ app/src/main/java/com/codementor/android/starwarsbattlefrontcommunity/network/APIServiceGenerator.java
@@ -1,6 +1,13 @@
 package com.codementor.android.starwarsbattlefrontcommunity.network;
 
+import android.util.Base64;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -17,9 +24,34 @@ public class APIServiceGenerator {
     private static Retrofit.Builder builder = new Retrofit.Builder().baseUrl(API_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create());
 
-    //defines annotated class or interface for API requests
-    public static <S> S createService(Class<S> serviceClass){
-        Retrofit retrofit = builder.client(httpClient.build()).build();
+    public static <S> S createService(Class<S> serviceClass) {
+        return createService(serviceClass, null, null);
+    }
+
+    public static <S> S createService(Class<S> serviceClass, String username, String password) {
+        if (username != null && password != null) {
+            String credentials = username + ":" + password;
+            final String basic =
+                    "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+
+            httpClient.addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Interceptor.Chain chain) throws IOException {
+                    Request original = chain.request();
+
+                    Request.Builder requestBuilder = original.newBuilder()
+                            .header("Authorization", basic)
+                    .header("Accept", "application/json")
+                    .method(original.method(), original.body());
+
+                    Request request = requestBuilder.build();
+                    return chain.proceed(request);
+                }
+            });
+        }
+
+        OkHttpClient client = httpClient.build();
+        Retrofit retrofit = builder.client(client).build();
         return retrofit.create(serviceClass);
     }
 }
